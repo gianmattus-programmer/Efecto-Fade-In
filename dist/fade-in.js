e71f20369e5412063c337e0d44c53016259582b0
Giancarlos Chavez Mattus <chavezmattus@gmail.com>
2025-04-11 01:10:21 -0500

    Update fade-in.js

--- dist/fade-in.js
+++ dist/fade-in.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.opacity = 0;
-  document.body.style.transition = "opacity 1s ease-in-out";
+  // Estilos iniciales
+  document.body.style.opacity = "0";
+  document.body.style.transform = "scale(0.98)";
+  document.body.style.filter = "blur(5px)";
+  document.body.style.transition = "opacity 1s ease, transform 1s ease, filter 1s ease";
 
+  // Cuando la página termine de cargar
   window.addEventListener("load", () => {
-    document.body.style.opacity = 1;
+    document.body.style.opacity = "1";
+    document.body.style.transform = "scale(1)";
+    document.body.style.filter = "blur(0)";
   });
 });
